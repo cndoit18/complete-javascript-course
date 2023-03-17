6f2555f60ec8fef4ed72b8d1e9d212eb9cac94c8
cndoit18 <cndoit18@outlook.com>
2023-03-17 13:56:41 +0800

    finish 09 Coding Challenge #2
    Signed-off-by: cndoit18 <cndoit18@outlook.com>

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -118,3 +118,53 @@ printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
 
 // #7
 console.log(`${(team1 > team2 && 'team2') || 'team1'} is more likely to win`);
+
+// Coding Challenge #2
+
+/* 
+Let's continue with our football betting app!
+
+1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
+      Odd of victory Bayern Munich: 1.33
+      Odd of draw: 3.25
+      Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
+
+BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+      {
+        Gnarby: 1,
+        Hummels: 1,
+        Lewandowski: 2
+      }
+
+GOOD LUCK 😀
+*/
+
+// #1
+for (const [index, player] of game.scored.entries()) {
+  console.log(`Goal ${index + 1}: ${player}`);
+}
+
+// #2
+function average(...odds) {
+  let sum = 0;
+  for (const odd of odds) sum += odd;
+  return sum / odds.length;
+}
+
+console.log(average(...Object.values(game.odds)));
+
+// #3
+for (const [team, odd] of Object.entries(game.odds)) {
+  console.log(
+    `Odd of ${(game?.[team] && 'victory ' + game?.[team]) ?? 'draw'}: ${odd}`
+  );
+}
+
+let scorers = {};
+for (const player of game.scored) {
+  scorers[player] = (scorers[player] ?? 0) + 1;
+}
+console.log(scorers);
