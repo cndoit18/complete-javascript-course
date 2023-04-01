a6ac536e7fe8f582a1756672f73f74fe3c1da8e1
cndoit18 <cndoit18@outlook.com>
2023-04-02 01:22:24 +0800

    finish 14 oop #2
    Signed-off-by: cndoit18 <cndoit18@outlook.com>

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -34,3 +34,42 @@ BWM.accelerate();
 BWM.accelerate();
 BWM.brake();
 BWM.brake();
+
+// Coding Challenge #2
+
+/* 
+1. Re-create challenge 1, but this time using an ES6 class;
+2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide by 1.6);
+3. Add a setter called 'speedUS' which sets the current speed in mi/h (but converts it to km/h before storing the value, by multiplying the input by 1.6);
+4. Create a new car and experiment with the accelerate and brake methods, and with the getter and setter.
+
+DATA CAR 1: 'Ford' going at 120 km/h
+
+GOOD LUCK 😀
+*/
+
+class CarCl {
+  constructor(mark, speed) {
+    this.mark = mark;
+    this.speed = speed;
+  }
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+  accelerate() {
+    this.speed += 10;
+    console.log(this.speed);
+  }
+  brake() {
+    this.speed -= 5;
+    console.log(this.speed);
+  }
+}
+
+let Ford = new CarCl('Ford', 120);
+Ford.accelerate();
+console.log(Ford.speedUS);
+console.log(Ford);
