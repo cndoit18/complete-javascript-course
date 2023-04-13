fa12cd81270e6a167f37eb139917f4a7c70c2313
cndoit18 <cndoit18@outlook.com>
2023-04-14 00:38:23 +0800

    finish 16 asynchronous #3
    Signed-off-by: cndoit18 <cndoit18@outlook.com>

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -144,24 +144,73 @@ function createImage(imgPath) {
   });
 }
 
-const wait = function (seconds, context) {
+// const wait = function (seconds, context) {
+//   return new Promise(function (resolve) {
+//     setTimeout(() => resolve(context), seconds * 1000);
+//   });
+// };
+
+// createImage('./img/img-1.jpg')
+//   .then(element => {
+//     return wait(2, element);
+//   })
+//   .then(element => {
+//     element.style.display = 'none';
+//     return createImage('./img/img-2.jpg');
+//   })
+//   .then(element => {
+//     return wait(2, element);
+//   })
+//   .then(element => {
+//     element.style.display = 'none';
+//   })
+//   .catch(e => console.error(e));
+
+// Coding Challenge #3
+
+/* 
+PART 1
+Write an async function 'loadNPause' that recreates Coding Challenge #2, this time using async/await (only the part where the promise is consumed). Compare the two versions, think about the big differences, and see which one you like more.
+Don't forget to test the error handler, and to set the network speed to 'Fast 3G' in the dev tools Network tab.
+
+PART 2
+1. Create an async function 'loadAll' that receives an array of image paths 'imgArr';
+2. Use .map to loop over the array, to load all the images with the 'createImage' function (call the resulting array 'imgs')
+3. Check out the 'imgs' array in the console! Is it like you expected?
+4. Use a promise combinator function to actually get the images from the array 😉
+5. Add the 'paralell' class to all the images (it has some CSS styles).
+
+TEST DATA: ['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']. To test, turn off the 'loadNPause' function.
+
+GOOD LUCK 😀
+*/
+
+const wait = function (seconds) {
   return new Promise(function (resolve) {
-    setTimeout(() => resolve(context), seconds * 1000);
+    setTimeout(resolve, seconds * 1000);
   });
 };
+async function loadNPause() {
+  try {
+    let element = await createImage('./img/img-1.jpg');
+    await wait(2);
+    element.style.display = 'none';
+    element = await createImage('./img/img-2.jpg');
+    await wait(2);
+    element.style.display = 'none';
+  } catch (e) {
+    console.error(e);
+  }
+}
+// loadNPause();
 
-createImage('./img/img-1.jpg')
-  .then(element => {
-    return wait(2, element);
-  })
-  .then(element => {
-    element.style.display = 'none';
-    return createImage('./img/img-2.jpg');
-  })
-  .then(element => {
-    return wait(2, element);
-  })
-  .then(element => {
-    element.style.display = 'none';
-  })
-  .catch(e => console.error(e));
+async function loadAll(...imgArr) {
+  try {
+    (await Promise.all(imgArr.map(v => createImage(v)))).map(elem => {
+      elem.classList.add('parallel');
+    });
+  } catch (e) {
+    console.error(e);
+  }
+}
+loadAll('img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg');
